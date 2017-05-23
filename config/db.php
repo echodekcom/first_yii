c12e82851eab5d48cbd227732edabbd8104abcb5
echodekcom <muhamafaiz.b@gmail.com>
2017-05-23 18:40:09 +0700

    My First Yii

---
+++ config/db.php
@@ -0,0 +1,9 @@
+<?php
+
+return [
+    'class' => 'yii\db\Connection',
+    'dsn' => 'mysql:host=localhost;dbname=first_yii_db',
+    'username' => 'root',
+    'password' => '',
+    'charset' => 'utf8',
+];
---
+++ models/Member.php
@@ -0,0 +1,54 @@
+<?php
+
+namespace app\models;
+
+use Yii;
+
+/**
+ * This is the model class for table "member".
+ *
+ * @property integer $id
+ * @property string $firstname
+ * @property string $lastname
+ * @property string $email
+ * @property string $address
+ * @property string $tel
+ */
+class Member extends \yii\db\ActiveRecord
+{
+    /**
+     * @inheritdoc
+     */
+    public static function tableName()
+    {
+        return 'member';
+    }
+
+    /**
+     * @inheritdoc
+     */
+    public function rules()
+    {
+        return [
+            [['firstname', 'lastname', 'email', 'tel'], 'required'],
+            [['address'], 'string'],
+            [['firstname', 'lastname', 'email'], 'string', 'max' => 100],
+            [['tel'], 'string', 'max' => 15],
+        ];
+    }
+
+    /**
+     * @inheritdoc
+     */
+    public function attributeLabels()
+    {
+        return [
+            'id' => 'ID',
+            'firstname' => 'Firstname',
+            'lastname' => 'Lastname',
+            'email' => 'Email',
+            'address' => 'Address',
+            'tel' => 'Tel',
+        ];
+    }
+}
